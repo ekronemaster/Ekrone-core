50077906a1ee2daee700539ff0d1c698f1baa3e9
ekronemaster <141001430+ekronemaster@users.noreply.github.com>
2023-09-18 10:59:10 +0800

    initial

--- scripts/deploy-node.sh
+++ scripts/deploy-node.sh
@@ -91,7 +91,7 @@ server {
     server_name         $DOMAIN;
 
     location / {
-        proxy_pass http://127.0.0.1:11898;
+        proxy_pass http://127.0.0.1:14081;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
